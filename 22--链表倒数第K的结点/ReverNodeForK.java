572899a03dbde28e7bc39e95f6a1964f13e8d3ec
commonsstring <m838206069@163.com>
2018-08-04 10:42:07 +0800

    倒数第K个结点 添加方法二，双指针法

--- 22--链表倒数第K的结点/ReverNodeForK.java
+++ 22--链表倒数第K的结点/ReverNodeForK.java
@@ -20,14 +20,57 @@ public class ReverNodeForK {
 		ListNodeS head = new ListNodeS(1);
 		ListNodeS head2 = new ListNodeS(2);
 		ListNodeS head3 = new ListNodeS(3);
+		ListNodeS head4 = new ListNodeS(4);
+		ListNodeS head5 = new ListNodeS(5);
 		head.next = head2;
 		head2.next = head3;
-		ListNodeS node = FindKthToTail(head, 1);
+		head3.next = head4;
+		head4.next = head5;
+		ListNodeS node = reverListSecond(head, 1);
 //		ListNodeS ss = reverList(head);
 //		System.out.println(ss.next.val);
 		System.out.println(node.val);
 //		System.out.println(getLength(head));
 	}
+	
+	
+	/**
+	 * 方法二：指针法, 限制只能遍历一次链表
+	 * 声明指针：M = K - 1, 下标从1开始
+	 *        N = 1, 指向头
+	 * 之后两指针同时向前移动，直到M到达尾部       	  
+	 * 返回N
+	 */
+	public static ListNodeS reverListSecond(ListNodeS head, int K){
+		//参数检查
+		if(head == null || K == 0) return null;
+		int len = getLength(head);
+		if(K > len){
+			return null;
+		}
+		//定位
+		ListNodeS c_p1 = head;
+		ListNodeS c_p2 = null;
+		//定位到K - 1位置
+		for(int i = 0; i < K - 1; i++){
+			if(c_p1.next != null){
+				c_p1 = c_p1.next;
+			}else{
+				return null;
+			}
+		}
+		c_p2 = head;
+		//next不等于空
+		while(c_p1.next != null){
+			c_p1 = c_p1.next;
+			c_p2 = c_p2.next;
+		}
+		return c_p2;
+	}
+	
+	
+	
+		
 	
 	/**
 	 * 反转链表
@@ -81,6 +124,7 @@ public class ReverNodeForK {
 			current = current.next;
 			count++;
 		}
+		
 		return parent;
 	}
 	
